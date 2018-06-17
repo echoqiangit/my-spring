9cda92edc6f54f78c54d044ca7a42cc2ae518672
echoqiangit <echoqian@gmail.com>
2018-06-16 22:37:01 -0500

    final js

--- src/main/java/com/example/myspring/MyWebServiceController.java
+++ src/main/java/com/example/myspring/MyWebServiceController.java
@@ -1,13 +1,28 @@
 package com.example.myspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
+@RequestMapping("/v1")
 public class MyWebServiceController {
 
 	@GetMapping("/login")
 	public String getLogin() {
 		return "login";
+	}
+	
+	
+	@PostMapping("/login")
+	public String postLogin(@RequestBody User user) {
+		System.out.println(user.getUsername());
+		String response =  "{\"User\":{\"Name\":\"" + user.getUsername() + "\"},\"AuthId\":\"abc\"}";
+		System.out.println(response);
+		return response;
 	}
 }
